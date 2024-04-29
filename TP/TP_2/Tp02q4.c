76749e950bb26e627a37b06168ce85801c54f43e
André Bassini <dev.andrebassini@gmail.com>
2024-04-29 17:01:05 -0300

    upload Tp02q4

--- TP/TP_2/Tp02q4.c
+++ TP/TP_2/Tp02q4.c
@@ -122,12 +122,3 @@ int main() {
 
   return 0;
 }
-
-/*
-Faça a inserção de alguns registros no final de um vetor e, em seguida, faça algumas pesquisas binárias. A chave primária de pesquisa será o atributo name. A entrada padrão é composta por duas partes onde a primeira é igual a entrada da primeira questão. As demais linhas correspondem a segunda parte. A segunda parte é composta por várias linhas. Cada uma possui um elemento que deve ser pesquisado no vetor. A última linha terá a palavra FIM. A saída padrão será composta por várias linhas contendo as palavras SIM/NAO para indicar se existe cada um dos elementos pesquisados.
-
-Além disso, crie um arquivo de log na pasta corrente com o nome matrícula_binaria.txt. com uma única linha contendo sua matrícula, tempo de execução do seu algoritmo e número de comparações. Todas as informações do arquivo de log devem ser separadas por uma tabulação '\t'.
-
-A entrada desta questão não está ordenada.
-
-*/
