820a1a782ec6b6f296621c956fa8ab5e1aaaca02
André Bassini <dev.andrebassini@gmail.com>
2024-08-05 12:00:05 -0300

    Update Exercises

--- 2024.2/VERDE/Palindromo.java
+++ 2024.2/VERDE/Palindromo.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Palindromo {
-    
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        String word = myObj.nextLine();
+        while (word != "FIM") {
+            word = word.toLowerCase();
+
+            StringBuilder sb = new StringBuilder(word);
+            String reversedString = sb.reverse().toString();
+
+            if (word.equals(reversedString)) {
+                System.out.println("SIM");
+            } else {
+                System.out.println("NAO");
+            }
+
+            word = myObj.nextLine();
+        }
+        myObj.close();
+    }
 }
--- 2024.2/VERDE/ParImpar.java
+++ 2024.2/VERDE/ParImpar.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class ParImpar {
-    
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); // Create a scanner object
+
+        int number = myObj.nextInt(); // Read User input
+        while (number != 0) {
+            if (number % 2 == 0)
+                System.out.println("P");
+            else
+                System.out.println("I");
+            number = myObj.nextInt(); // Read User input
+        }
+        myObj.close();
+    }
 }
