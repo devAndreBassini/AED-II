dc07d03b90afcccba9d2bc81811c85b53813309f
André Bassini <dev.andrebassini@gmail.com>
2024-08-12 17:25:16 -0300

    Update TP01

--- 2024.2/TP/01/Ciframento.c
+++ 2024.2/TP/01/Ciframento.c
@@ -6,25 +6,6 @@
 
 const int key = 3;
 
-char *ciframento(char *word){
-    int lenght = strlen(word);
-
-    for(int i = 0; i < lenght; i++){
-        char newLetter = word[i] + key;
-
-        if(word[i] == tolower(word[i])){
-            if(newLetter%'a' > 25)
-            word[i] = 'a' + (newLetter%'a' - 26);
-        } else if(word[i] == toupper(word[i])){
-            if(newLetter%'A' > 25)
-            word[i] = 'A' + (newLetter%'A' - 26);
-        }
-
-        word[i] = newLetter;
-    }  
-    return word;  
-}
-
 int main(){
     char word[80];
 
@@ -34,8 +15,11 @@ int main(){
         if(strcmp(word, "FIM") == 0)
             break;
 
-        letter(word);
-        // printf("%s\n", ciframento(word));
+        for(int i = 0; i < strlen(word); i++){
+            word[i] += key;
+        }
+        
+        printf("%s\n", word);
     }
     return 0;
 }
