ee2b5e26065eb5a31b6d5714627a50766431b53d
devAndreBassini <dev.andrebassini@gmail.com>
2025-03-11 13:40:34 -0300

    TP01Q11 -  Refactor substring logic in Substring.java

--- 2024.2/TPs/01/Q11/Substring.java
+++ 2024.2/TPs/01/Q11/Substring.java
@@ -14,28 +14,31 @@ public class Substring {
             boolean isIn = false;
 
             for (int i = 0; i < input.length(); i++) {
-                if (substring[subStringID] == null)
-                    substring[subStringID] = "";
+                if (substring[subStringID] == null) substring[subStringID] = "";
 
                 substring[subStringID] += input.charAt(i);
 
                 if (i + 1 < input.length())
                     isIn = substring[subStringID].contains(String.valueOf(input.charAt(i + 1)));
-                else
+                else 
                     subStringID++;
 
-                if (isIn)
-                    subStringID++;
 
+                if(isIn){
+                    i = subStringID;
+                    subStringID++;
+                    isIn = false;
+                }
+                
             }
-
+            
             for (int i = 0; i < subStringID; i++) {
                 if (substring[i] != null && maior < substring[i].length()) {
                     maior = substring[i].length();
                     maisLongaID = i;
                 }
             }
-            
+
             System.out.println(substring[maisLongaID].length());
         }
         sc.close();
